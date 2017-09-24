ca2b1da335d599e0dffca7775957bec45c92d816
gluedog <billionaire@billionairetoken.com>
2017-09-25 01:06:38 +0300

    latest version, in-development
    Code finished, not tested.

--- Burner.sol
+++ Burner.sol
@@ -10,22 +10,22 @@ pragma solidity ^0.4.0;
 
 contract XBL_ERC20Wrapper
 {
-	function transferFrom(address from, address to, uint value) returns (bool success);
-	function transfer(address _to, uint _value) returns (bool success);
-	function allowance(address _owner, address _spender) constant returns (uint256 remaining);
-	function burn(uint256 _value) returns (bool success);
-	function balanceOf(address _owner) constant returns (uint256 balance);
-	function totalSupply() constant returns (uint256 total_supply);
+    function transferFrom(address from, address to, uint value) returns (bool success);
+    function transfer(address _to, uint _value) returns (bool success);
+    function allowance(address _owner, address _spender) constant returns (uint256 remaining);
+    function burn(uint256 _value) returns (bool success);
+    function balanceOf(address _owner) constant returns (uint256 balance);
+    function totalSupply() constant returns (uint256 total_supply);
     function burnFrom(address _from, uint256 _value) returns (bool success);
 }
 
 contract TheBurner
 {
-	XBL_ERC20Wrapper ERC20_CALLS;
-	uint8 public extra_bonus; /* The percentage of extra coins that the burner will reward people for. */
+    XBL_ERC20Wrapper ERC20_CALLS;
+    uint8 public extra_bonus; /* The percentage of extra coins that the burner will reward people for. */
 
-	address public burner_addr;
-	address public raffle_addr;
+    address public burner_addr;
+    address public raffle_addr;
     address owner_addr;
     address XBLContract_addr;
 
@@ -38,32 +38,32 @@ contract TheBurner
         owner_addr = msg.sender;
     }
 
-	modifier onlyOwner() 
-	{
-    	require (msg.sender == owner_addr);
-    	_;
-  	}
+    modifier onlyOwner() 
+    {
+        require (msg.sender == owner_addr);
+        _;
+    }
 
-  	function setRaffleAddress(address _raffle_addr) onlyOwner
-  	{
-  		/* Allows the owner to set the raffle address */
-  		raffle_addr = _raffle_addr;
-  	}
+    function setRaffleAddress(address _raffle_addr) onlyOwner
+    {
+        /* Allows the owner to set the raffle address */
+        raffle_addr = _raffle_addr;
+    }
 
     function getPercent(uint8 percent, uint256 number) returns (uint256 result)
     {
         return number * percent / 100;
     }
 
-	function registerBurn(uint256 tokens_registered) returns (int8 registerBurn_STATUS)
-	{
+    function registerBurn(uint256 tokens_registered) returns (int8 registerBurn_STATUS)
+    {
         /* This will just throw if bad input */
-		address user_addr = msg.sender;
+        address user_addr = msg.sender;
 
         uint256 actual_allowance = ERC20_CALLS.allowance(user_addr, burner_addr);
         require (actual_allowance >= tokens_registered); // Is the user bullshitting us?
 
-		uint256 own_supply = ERC20_CALLS.balanceOf(burner_addr);
+        uint256 own_supply = ERC20_CALLS.balanceOf(burner_addr);
         uint256 eligible_reward = tokens_registered + getPercent(extra_bonus, tokens_registered);
         require (eligible_reward <= own_supply); // Do we have enough tokens to give out?
 
@@ -73,5 +73,5 @@ contract TheBurner
         ERC20_CALLS.burnFrom(user_addr, tokens_registered);
         ERC20_CALLS.transfer(user_addr, eligible_reward);
 
-	}
+    }
 }
