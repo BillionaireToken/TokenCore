18114481b9f8a533604133eedf16d3826594f6ec
gluedog <billionaire@billionairetoken.com>
2017-09-25 01:05:02 +0300

    latest-version, in development
    Rather finished code, untested.

--- Burner.sol
+++ Burner.sol
@@ -1,20 +1,9 @@
-/* The Burner v0.1 
+/* The Burner v0.1, pre-release.
 *  ~by gluedog
 *
 * The Burner is Billionaire Token's version of a "Faucet" - an evil, twisted Faucet. 
 * Just like a Faucet, people can use it to get some extra coins. 
 * Unlike a Faucet, the Burner will also burn coins and reduce the maximum supply in the process of giving people extra coins.
-*
-* The Burner must:
-*
-*
-* 1. Receive XBL Tokens from the Raffle
-* 2. Have a registerBurn() function.
-* 3. The registerBurn() function:
-*     - Checks allowance from users.
-* 	  - Checks own supply. If own_supply <= user_submitted_tokens+extra_bonus it will use revert() to revert the Transaction.
-* 	  - Calls burnFrom() to burn the users tokens.
-* 	  - Sends the user their burned tokens +extra_bonus.
 */
 
 pragma solidity ^0.4.0;
@@ -27,19 +16,32 @@ contract XBL_ERC20Wrapper
 	function burn(uint256 _value) returns (bool success);
 	function balanceOf(address _owner) constant returns (uint256 balance);
 	function totalSupply() constant returns (uint256 total_supply);
+    function burnFrom(address _from, uint256 _value) returns (bool success);
 }
 
 contract TheBurner
 {
 	XBL_ERC20Wrapper ERC20_CALLS;
 	uint8 public extra_bonus; /* The percentage of extra coins that the burner will reward people for. */
-	address burner_addr;
-	address raffle_addr;
+
+	address public burner_addr;
+	address public raffle_addr;
+    address owner_addr;
+    address XBLContract_addr;
+
+    function TheBurner()
+    {
+        XBLContract_addr = 0x49AeC0752E68D0282Db544C677f6BA407BA17ED7;
+        ERC20_CALLS = XBL_ERC20Wrapper(XBLContract_addr);
+        extra_bonus = 5; /* 5% reward for burning your own coins, provided the burner has enough. */
+        burner_addr = address(this);
+        owner_addr = msg.sender;
+    }
 
 	modifier onlyOwner() 
-    	{
-    		require (msg.sender == owner_addr);
-    		_;
+	{
+    	require (msg.sender == owner_addr);
+    	_;
   	}
 
   	function setRaffleAddress(address _raffle_addr) onlyOwner
@@ -48,17 +50,28 @@ contract TheBurner
   		raffle_addr = _raffle_addr;
   	}
 
-	function TheBurner()
-	{
-		XBLContract_addr = 0x49AeC0752E68D0282Db544C677f6BA407BA17ED7;
-		ERC20_CALLS = XBL_ERC20Wrapper(XBLContract_addr);
-		extra_bonus = 5; /* 5% reward for burning your own coins, provided the burner has enough. */
-		burner_addr = address(this);
-	}
+    function getPercent(uint8 percent, uint256 number) returns (uint256 result)
+    {
+        return number * percent / 100;
+    }
 
 	function registerBurn(uint256 tokens_registered) returns (int8 registerBurn_STATUS)
 	{
-		address user = msg.sender;
-		uint256 own_supply = ERC20_CALLS.balanceOf(user_addr, raffle_addr);
+        /* This will just throw if bad input */
+		address user_addr = msg.sender;
+
+        uint256 actual_allowance = ERC20_CALLS.allowance(user_addr, burner_addr);
+        require (actual_allowance >= tokens_registered); // Is the user bullshitting us?
+
+		uint256 own_supply = ERC20_CALLS.balanceOf(burner_addr);
+        uint256 eligible_reward = tokens_registered + getPercent(extra_bonus, tokens_registered);
+        require (eligible_reward <= own_supply); // Do we have enough tokens to give out?
+
+        /* Reaching this point means we can give out the reward */
+
+        /* Burn their tokens and give them their reward */
+        ERC20_CALLS.burnFrom(user_addr, tokens_registered);
+        ERC20_CALLS.transfer(user_addr, eligible_reward);
+
 	}
 }
